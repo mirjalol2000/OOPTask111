f1d1bef3ecc4fe29b7d12efb4187f9d803392605
hicadok105@ridteam.com <hicadok105@ridteam.com>
2024-03-21 22:59:05 +0500

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTask1
+{
+    internal class Book
+    {
+        private string author;
+        private string value = "Abdulla Qodiriy";
+        public string NameOfBook { get; set; }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = ChangeAuthor(author);
+            }
+
+        }
+        public int CostOfBook { get; set; }
+
+        public Book()
+        {
+            NameOfBook = "O'tkan kunlar";
+        }
+
+        public string ChangeAuthor(string author)
+        {
+            return author;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOPTask1;
+
+Book newbook = new Book();
+newbook.CostOfBook = 500;
+
+Console.WriteLine(newbook.ChangeAuthor("Usmon Nosir"));
+
+//Console.WriteLine("Hello, World!");
